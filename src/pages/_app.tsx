ba52792639c374d13c916e5b4c9cee3805bd1b93
smalandrey67 <smalandrey67@ukr.net>
2023-05-24 15:10:01 +0300

    connect font, manage styles. Get back to npm

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
 import type { AppProps } from "next/app";
 
-import "@/styles/globals.css";
+import { Roboto_Flex } from "@next/font/google";
+
+import "@/styles/globals.scss";
+
+const font = Roboto_Flex({
+	subsets: ["latin"],
+	weight: ["400", "500", "700"]
+});
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<style jsx global>
+				{`
+					:root {
+						--roboto-flex-font: ${font.style.fontFamily};
+					}
+				`}
+			</style>
+			<Component {...pageProps} />
+		</>
+	);
 }
